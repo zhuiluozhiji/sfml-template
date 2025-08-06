e816e38f48050a4f4de162d805108c76beb7570f
zhuiluozhiji <781000194@qq.com>
2025-08-06 16:28:23 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,21 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+    window.setVerticalSyncEnabled(true); // call it once, after creating the window
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
 
     return 0;
