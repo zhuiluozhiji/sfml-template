81e91535b5d88130541b9cc6d59ab0d95e2fbf20
zhuiluozhiji <781000194@qq.com>
2025-08-06 13:55:00 +0800

    切换到sfml-template路径

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,23 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            if (event->is<sf::Event::Closed>())
-            {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
