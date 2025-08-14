be3ee4fd1714f770b35d7e2573d5d1a07ab75522
zhuiluozhiji <781000194@qq.com>
2025-08-14 17:18:44 +0800

    加速度和阻本的方块移动（帧率独立）

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/System/Vector2.hpp>
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
@@ -12,14 +13,22 @@ int main()
 {
     // create the window
     RenderWindow window(VideoMode(800, 600), "My window");
-    window.setFramerateLimit(60); // limit the frame rate to 60 FPS
-
+    //window.setFramerateLimit(60); // limit the frame rate to 60 FPS
+    //dt at 60 FPS is 1/60 = 0.016 seconds
     Clock clock;
     float dt;
     float multiplier = 60.f;
 
     RectangleShape shape(Vector2f(50.f, 50.f));
     shape.setFillColor(Color::White);
+
+    Vector2f currentVelocity;
+    Vector2f direction;
+    float maxVelocity = 25.f;
+    float acceleration = 1.f;
+    float drag = 0.5f;
+
+
 
 
     // run the program as long as the window is open
@@ -35,26 +44,67 @@ int main()
         }
 
         dt = clock.restart().asSeconds(); // get the time elapsed since the last frame
+//UPDATE
+//acceleration
+        direction = Vector2f(0.f,0.f); 
+        
         if(Keyboard::isKeyPressed(Keyboard::A))
         {
-            shape.move(-10.f * dt * multiplier, 0.f);
+            direction.x = -1.f;
+            if(currentVelocity.x > -maxVelocity)
+            {
+                currentVelocity.x += acceleration * direction.x * dt * multiplier;
+            }
         }
 
         if(Keyboard::isKeyPressed(Keyboard::D))
         {
-            shape.move(10.f * dt * multiplier, 0.f);
+            direction.x = 1.f;
+            if(currentVelocity.x < maxVelocity)
+            {
+                currentVelocity.x += acceleration * direction.x * dt * multiplier;
+            }
         }
 
         if(Keyboard::isKeyPressed(Keyboard::W))
         {
-            shape.move(0.f, -10.f * dt * multiplier);
+            direction.y = -1.f;
+            if(currentVelocity.y > -maxVelocity)
+            {
+                currentVelocity.y += acceleration * direction.y * dt * multiplier;
+            }
         }
 
         if(Keyboard::isKeyPressed(Keyboard::S))
         {
-            shape.move(0.f, 10.f * dt * multiplier);
+            direction.y = 1.f;
+            if(currentVelocity.y < maxVelocity)
+            {
+                currentVelocity.y += acceleration * direction.y * dt * multiplier;
+            }
         }
-
+//drag
+        if(currentVelocity.x > 0.f)
+        {
+            currentVelocity.x -= drag * dt * multiplier;
+            if(currentVelocity.x < 0.f) currentVelocity.x = 0.f;
+        }
+        else if (currentVelocity.x < 0.f)
+        {
+            currentVelocity.x += drag * dt * multiplier;
+            if(currentVelocity.x > 0.f) currentVelocity.x = 0.f;
+        }
+        if(currentVelocity.y > 0.f)
+        {
+            currentVelocity.y -= drag * dt * multiplier;
+            if(currentVelocity.y < 0.f) currentVelocity.y = 0.f;
+        }
+        else if (currentVelocity.y < 0.f)
+        {
+            currentVelocity.y += drag * dt * multiplier;
+            if(currentVelocity.y > 0.f) currentVelocity.y = 0.f;
+        }
+        shape.move(currentVelocity * dt * multiplier); // move the shape based on current velocity
         // clear the window with black color
         window.clear(Color::Black);
 
